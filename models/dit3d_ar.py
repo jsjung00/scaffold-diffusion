b1e869de15646c8692a962ef79502e9a8af7cf95
Justin <jsjung00@explorer.falcon-typhon.ts.net>
2025-08-18 20:32:15 -0700

    add dit3d ar code

---
+++ models/dit3d_ar.py
@@ -0,0 +1,402 @@
+import math
+import typing
+
+import flash_attn
+import flash_attn.layers.rotary
+import huggingface_hub
+import omegaconf
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from einops import rearrange
+import numpy as np
+
+# Flags required to enable jit fusion kernels
+torch._C._jit_set_profiling_mode(False)
+torch._C._jit_set_profiling_executor(False)
+torch._C._jit_override_can_fuse_on_cpu(True)
+torch._C._jit_override_can_fuse_on_gpu(True)
+
+
+def bias_dropout_add_scale(
+    x: torch.Tensor,
+    bias: typing.Optional[torch.Tensor],
+    scale: torch.Tensor,
+    residual: typing.Optional[torch.Tensor],
+    prob: float,
+    training: bool,
+) -> torch.Tensor:
+    if bias is not None:
+        out = scale * F.dropout(x + bias, p=prob, training=training)
+    else:
+        out = scale * F.dropout(x, p=prob, training=training)
+
+    if residual is not None:
+        out = residual + out
+    return out
+
+
+def get_bias_dropout_add_scale(training):
+    def _bias_dropout_add(x, bias, scale, residual, prob):
+        return bias_dropout_add_scale(x, bias, scale, residual, prob, training)
+
+    return _bias_dropout_add
+
+
+# function overload
+def modulate(x: torch.Tensor, shift: torch.Tensor, scale: torch.Tensor) -> torch.Tensor:
+    return x * (1 + scale) + shift
+
+
+@torch.jit.script
+def bias_dropout_add_scale_fused_train(
+    x: torch.Tensor,
+    bias: typing.Optional[torch.Tensor],
+    scale: torch.Tensor,
+    residual: typing.Optional[torch.Tensor],
+    prob: float,
+) -> torch.Tensor:
+    return bias_dropout_add_scale(x, bias, scale, residual, prob, True)
+
+
+@torch.jit.script
+def bias_dropout_add_scale_fused_inference(
+    x: torch.Tensor,
+    bias: typing.Optional[torch.Tensor],
+    scale: torch.Tensor,
+    residual: typing.Optional[torch.Tensor],
+    prob: float,
+) -> torch.Tensor:
+    return bias_dropout_add_scale(x, bias, scale, residual, prob, False)
+
+
+@torch.jit.script
+def modulate_fused(
+    x: torch.Tensor, shift: torch.Tensor, scale: torch.Tensor
+) -> torch.Tensor:
+    return modulate(x, shift, scale)
+
+
+###
+# Positional Encoding
+###
+def get_3d_sincos_pos_embed(embed_dim, grid_size, cls_token=False, extra_tokens=0):
+    """
+    grid_size: int of the grid height and width
+    return:
+    pos_embed: [grid_size*grid_size, embed_dim] or [1+grid_size*grid_size, embed_dim] (w/ or w/o cls_token)
+    #TODO: verify that this output shape is wrong and that the output shape should be [grid_size^3, embed_dim]
+    """
+    print("grid_size:", grid_size)
+
+    grid_x = np.arange(grid_size, dtype=np.float32)
+    grid_y = np.arange(grid_size, dtype=np.float32)
+    grid_z = np.arange(grid_size, dtype=np.float32)
+
+    grid = np.meshgrid(grid_x, grid_y, grid_z, indexing="ij")  # here y goes first
+    grid = np.stack(grid, axis=0)
+
+    grid = grid.reshape([3, 1, grid_size, grid_size, grid_size])
+    pos_embed = get_3d_sincos_pos_embed_from_grid(embed_dim, grid)
+
+    if cls_token and extra_tokens > 0:
+        pos_embed = np.concatenate(
+            [np.zeros([extra_tokens, embed_dim]), pos_embed], axis=0
+        )
+    return pos_embed
+
+
+def get_3d_sincos_pos_embed_from_grid(embed_dim, grid):
+    assert embed_dim % 3 == 0
+
+    # use half of dimensions to encode grid_h
+    emb_x = get_1d_sincos_pos_embed_from_grid(embed_dim // 3, grid[0])  # (X, Y, Z, D/3)
+    emb_y = get_1d_sincos_pos_embed_from_grid(embed_dim // 3, grid[1])  # (X, Y, Z, D/3)
+    emb_z = get_1d_sincos_pos_embed_from_grid(embed_dim // 3, grid[2])  # (X, Y, Z, D/3)
+
+    emb = np.concatenate([emb_x, emb_y, emb_z], axis=-1)  # (X, Y, Z, D)
+    return emb
+
+
+def get_1d_sincos_pos_embed_from_grid(embed_dim, pos):
+    """
+    embed_dim: output dimension for each position
+    pos: a list of positions to be encoded: size (M,)
+    out: (M, D)
+    """
+    assert embed_dim % 2 == 0
+    omega = np.arange(embed_dim // 2, dtype=np.float64)
+    omega /= embed_dim / 2.0
+    omega = 1.0 / 10000**omega  # (D/2,)
+
+    orig_shape = pos.shape  # (X,Y,Z)
+
+    pos_flat = pos.reshape(-1)  # (M,)
+    out = np.einsum("m,d->md", pos_flat, omega)  # (M, D/2), outer product
+
+    emb_sin = np.sin(out)  # (M, D/2)
+    emb_cos = np.cos(out)  # (M, D/2)
+
+    emb = np.concatenate([emb_sin, emb_cos], axis=1)  # (M, D)
+
+    # reshape back to (X,Y,Z, D)
+    emb = emb.reshape(orig_shape + (embed_dim,))
+    return emb
+
+
+###
+# Layers
+###
+class LayerNorm(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones([dim]))
+        self.dim = dim
+
+    def forward(self, x):
+        with torch.cuda.amp.autocast(enabled=False):
+            x = F.layer_norm(x.float(), [self.dim])
+
+        return x * self.weight[None, None, :]
+
+
+class TimestepEmbedder(nn.Module):
+    """
+    Embeds scalar timesteps into vector representations
+    """
+
+    def __init__(self, hidden_size, frequency_embedding_size=256):
+        super().__init__()
+        self.mlp = nn.Sequential(
+            nn.Linear(frequency_embedding_size, hidden_size, bias=True),
+            nn.SiLU(),
+            nn.Linear(hidden_size, hidden_size, bias=True),
+        )
+        self.frequency_embedding_size = frequency_embedding_size
+
+    def timestep_embedding(self, t, dim, max_period=10000):
+        """
+        Create sinusoidal timestep embeddings
+
+        :param t: a 1-D Tensor of N indices, one per batch element.
+                      These may be fractional.
+        :param dim: the dimension of the output.
+        :param max_period: controls the minimum frequency of the embeddings.
+        :return: an (N, D) Tensor of positional embeddings.
+        """
+        half = dim // 2
+        freqs = torch.exp(
+            -math.log(max_period)
+            * torch.arange(start=0, end=half, dtype=torch.float32)
+            / half
+        ).to(device=t.device)
+        args = t[:, None].float() * freqs[None]
+        embedding = torch.cat([torch.cos(args), torch.sin(args)], dim=-1)
+        if dim % 2:
+            embedding = torch.cat(
+                [embedding, torch.zeros_like(embedding[:, :1])], dim=-1
+            )
+        return embedding
+
+    def forward(self, t):
+        t_freq = self.timestep_embedding(t, self.frequency_embedding_size)
+        t_emb = self.mlp(t_freq)
+        return t_emb
+
+
+class EmbeddingLayer(nn.Module):
+    def __init__(self, dim, vocab_dim):
+        super().__init__()
+        self.embedding = nn.Parameter(torch.empty((vocab_dim, dim)))
+        torch.nn.init.kaiming_uniform_(self.embedding, a=math.sqrt(5))
+
+    def forward(self, x):
+        return self.embedding[x]
+
+
+###
+# Core Model
+####
+
+
+class DDiTBlock(nn.Module):
+    def __init__(self, dim, n_heads, mlp_ratio=4, dropout=0.1):
+        super().__init__()
+        self.n_heads = n_heads
+
+        self.norm1 = LayerNorm(dim)
+        self.attn_qkv = nn.Linear(dim, 3 * dim, bias=False)
+        self.attn_out = nn.Linear(dim, dim, bias=False)
+        self.dropout1 = nn.Dropout(dropout)
+
+        self.norm2 = LayerNorm(dim)
+        self.mlp = nn.Sequential(
+            nn.Linear(dim, mlp_ratio * dim, bias=True),
+            nn.GELU(approximate="tanh"),
+            nn.Linear(mlp_ratio * dim, dim, bias=True),
+        )
+        self.dropout2 = nn.Dropout(dropout)
+        self.dropout = dropout
+
+    def _get_bias_dropout_scale(self):
+        if self.training:
+            return bias_dropout_add_scale_fused_train
+        else:
+            return bias_dropout_add_scale_fused_inference
+
+    def forward(self, x, pad_mask, seqlens=None):
+        '''
+        pad_mask: (B,L) boolean mask. 1 if not pad. 
+
+        We handle causal masking in addition to pad masking
+        '''
+        batch_size, seq_len = x.shape[0], x.shape[1]
+
+        bias_dropout_scale_fn = self._get_bias_dropout_scale()
+
+        # attention operation
+        x_skip = x
+        x = self.norm1(x)
+
+        qkv = self.attn_qkv(x)
+        qkv = rearrange(
+            qkv, "b s (three h d) -> b three h s d", three=3, h=self.n_heads
+        )
+        q, k, v = qkv[:, 0], qkv[:, 1], qkv[:, 2] # (b h s d)
+
+        attention_mask = torch.einsum('bi,bj->bij', pad_mask, pad_mask).bool() #(b,s,s)
+        causal_mask = torch.tril(torch.ones(seq_len, seq_len), diagonal=0)
+        causal_mask = torch.broadcast_to(causal_mask, attention_mask.shape).bool()
+        attention_mask = attention_mask & causal_mask
+
+        attention_mask = attention_mask.unsqueeze(1).expand(-1, self.n_heads, -1, -1)
+        x = torch.nn.functional.scaled_dot_product_attention(q,k,v, attn_mask=attention_mask, dropout_p=0.0)
+
+        x = rearrange(x, "b h s d -> b s (h d)", b = batch_size, h=self.n_heads)
+        #x = rearrange(x, "(b s)
+        #  h d -> b s (h d)", b=batch_size)
+
+        x_out = self.attn_out(x)
+
+        x = bias_dropout_scale_fn(
+            x_out, None, torch.ones_like(x_out), x_skip, self.dropout
+        )
+
+        x_out = self.mlp(self.norm2(x))
+        # mlp operation
+        x = bias_dropout_scale_fn(
+            x_out,
+            None,
+            torch.ones_like(x_out),
+            x,
+            self.dropout,
+        )
+        return x
+
+
+class DDitFinalLayer(nn.Module):
+    def __init__(self, hidden_size, out_channels):
+        super().__init__()
+        self.norm_final = LayerNorm(hidden_size)
+        self.linear = nn.Linear(hidden_size, out_channels)
+        self.linear.weight.data.zero_()
+        self.linear.bias.data.zero_()
+
+    def forward(self, x):
+        x = self.norm_final(x)
+        x = self.linear(x)
+        return x
+
+
+class DITAR(nn.Module, huggingface_hub.PyTorchModelHubMixin):
+    def __init__(self, config, vocab_size: int):
+        super().__init__()
+        if type(config) == dict:
+            config = omegaconf.OmegaConf.create(config)
+
+        self.config = config
+        self.vocab_size = vocab_size
+
+        self.vocab_embed = EmbeddingLayer(config.model.hidden_size, vocab_size)
+
+        self.learned_pos_embeddings = config.model.learned_pos_embeddings
+
+        self.seq_len = config.data.max_seq_len
+        self.voxel_side_len = config.data.voxel_side_len
+
+        self.sigma_map = TimestepEmbedder(config.model.cond_dim)
+        if self.learned_pos_embeddings:
+            self.pos_embed = nn.Embedding(num_embeddings=self.voxel_side_len**3, embedding_dim=config.model.hidden_size)
+        else:
+            self.pos_embed = nn.Parameter(
+                torch.zeros(
+                    self.voxel_side_len,
+                    self.voxel_side_len,
+                    self.voxel_side_len,
+                    config.model.hidden_size,
+                ),
+                requires_grad=False,
+            )
+            self.initialize_weights()
+
+        blocks = []
+        for _ in range(config.model.n_blocks):
+            blocks.append(
+                DDiTBlock(
+                    config.model.hidden_size,
+                    config.model.n_heads,
+                    dropout=config.model.dropout,
+                )
+            )
+
+        self.blocks = nn.ModuleList(blocks)
+
+        self.output_layer = DDitFinalLayer(
+            config.model.hidden_size, vocab_size
+        )
+        self.scale_by_sigma = config.model.scale_by_sigma
+
+
+    def initialize_weights(self):
+        pos_embed = get_3d_sincos_pos_embed(
+            self.pos_embed.shape[-1], self.voxel_side_len
+        ).squeeze()
+        self.pos_embed.data.copy_(torch.from_numpy(pos_embed).float())
+
+    def _get_bias_dropout_scale(self):
+        if self.training:
+            return bias_dropout_add_scale_fused_train
+        else:
+            return bias_dropout_add_scale_fused_inference
+
+    def forward(self, indices, token_pos, attention_mask):
+        """
+        indices: sequence tokens, padded to some constant length L. Tensor of shape (B, L)
+        token_pos: the (x,y,z) position of all the sequence tokens. Tensor of shape (B, L, 3)
+        attention_mask: (torch.Tensor) Boolean of shape (B,L) 0's where pad tokens
+
+        Important that we remove [PAD] tokens + pos embedding from computation
+        """
+        x = self.vocab_embed(indices)
+
+        # currenlty use sin positional embedding
+        x_coord = token_pos[..., 0]
+        y_coord = token_pos[..., 1]
+        z_coord = token_pos[..., 2]
+
+        if self.learned_pos_embeddings:
+            flattened_coord = (self.voxel_side_len**2)*x_coord + (self.voxel_side_len)*y_coord + z_coord
+            pos_embed = self.pos_embed(flattened_coord)
+        else:
+            pos_embed = self.pos_embed[x_coord, y_coord, z_coord]  # (B, L, hidden_dim)
+
+        x = x + pos_embed
+
+        seq_len = indices.shape[1]
+       
+        with torch.cuda.amp.autocast(dtype=torch.bfloat16):
+            for i in range(len(self.blocks)):
+                x = self.blocks[i](x, attention_mask, seqlens=None)
+            x = self.output_layer(x)
+
+        return x
